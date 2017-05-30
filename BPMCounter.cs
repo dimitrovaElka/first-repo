a9ec41e9f10ae9116f7255c7b65fcd16d9eb6fa6
dimitrovaElka <dimitrova_elka@abv.bg>
2017-05-30 16:24:27 +0300

    Add files via upload

---
+++ BPMCounter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BPMCounter
+{
+    public class BPMCounter
+    {
+        public static void Main()
+        {
+            var beatsPerMinute = int.Parse(Console.ReadLine());
+            var numberOfBits = int.Parse(Console.ReadLine());
+            double bars = Math.Round(numberOfBits / 4.0, 1);
+            double lengthInSeconds = numberOfBits * 60.0 / beatsPerMinute;
+
+            var minutes = Math.Floor(lengthInSeconds / 60);
+            var seconds = Math.Floor(lengthInSeconds % 60);
+
+            Console.WriteLine($"{bars} bars - {minutes}m {seconds}s");
+        }
+    }
+}
---
+++ DNASequences.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DNASequences
+{
+    public class DNASequences
+    {
+        public static void Main()
+        {
+            int matchSum = int.Parse(Console.ReadLine());
+            string nucleicAcid = "ACGT";
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    for (int k = 0; k < 4; k++)
+                    {
+                        var casing = "X";
+                        if ((i + 1) + (j + 1) + (k + 1) >= matchSum)
+                        {
+                            casing = "O";
+                        }
+                        var currentNucleicAcidSequence = casing + nucleicAcid[i] + nucleicAcid[j] + nucleicAcid[k] + casing;
+                        Console.Write(currentNucleicAcidSequence + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
